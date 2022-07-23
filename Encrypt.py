ee57d6f9eefdaad74cacbbdb2cbec2e8dd16f071
NIRONMAN <108979457+NIRONMAN@users.noreply.github.com>
2022-07-23 12:55:38 +0530

    Update and rename Encrypt to Encrypt.py

--- Encrypt.py
+++ Encrypt.py
@@ -1,12 +1,13 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-#direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 def encrypt(text,shift):
     newtext=""
     for i in range(0,len(text)):
-        newtext=newtext+alphabet[alphabet.index(text[i])+shift]
+        if (alphabet.index(text[i])+shift)>25:
+            newtext+=alphabet[alphabet.index(text[i])+shift-26]
+        else:    
+            newtext+=alphabet[alphabet.index(text[i])+shift]
     print(newtext)  
 encrypt(text,shift)
